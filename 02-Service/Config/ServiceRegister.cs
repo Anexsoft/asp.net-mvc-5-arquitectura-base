17d369f9d13661b918edfd783eaea9eb50afc22e
Eduardo <hitogoroshi@outlook.com>
2017-04-10 23:28:38 -0500

    update service register

--- 02-Service/Config/ServiceRegister.cs
+++ 02-Service/Config/ServiceRegister.cs
@@ -1,4 +1,5 @@
 ﻿using LightInject;
+using Model.Auth;
 using Model.Domain;
 using Persistence.DbContextScope;
 using Persistence.Repository;
@@ -12,7 +13,11 @@ namespace Service.Config
             var ambientDbContextLocator = new AmbientDbContextLocator();
 
             container.Register<IDbContextScopeFactory>((x) => new DbContextScopeFactory(null));
-            container.Register(typeof(IRepository<>), typeof(Repository<>), new PerScopeLifetime());
+
+            container.Register<IRepository<ApplicationUser>>((x) => new Repository<ApplicationUser>(ambientDbContextLocator));
+            container.Register<IRepository<Course>>((x) => new Repository<Course>(ambientDbContextLocator));
+            container.Register<IRepository<Student>>((x) => new Repository<Student>(ambientDbContextLocator));
+            container.Register<IRepository<StudentPerCourse>>((x) => new Repository<StudentPerCourse>(ambientDbContextLocator));
 
             container.Register<IStudentService, StudentService>();
             container.Register<IStudentPerCourseService, StudentPerCourseService>();
