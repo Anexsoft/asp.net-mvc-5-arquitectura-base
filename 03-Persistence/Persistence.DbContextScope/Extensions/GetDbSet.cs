59fa20cfcca9af5c15910ace3906b1348e7bb8d7
Eduardo <hitogoroshi@outlook.com>
2017-06-27 17:45:09 -0500

    Update Scope
    Now you execute SQL Query, retrieve data as object and execute
    procedure.

--- 03-Persistence/Persistence.DbContextScope/Extensions/GetDbSet.cs
+++ 03-Persistence/Persistence.DbContextScope/Extensions/GetDbSet.cs
@@ -1,5 +1,6 @@
 ﻿using Persistence.DatabaseContext;
 using System.Data.Entity;
+using System.Linq;
 
 namespace Persistence.DbContextScope.Extensions
 {
@@ -13,6 +14,42 @@ namespace Persistence.DbContextScope.Extensions
         public static DbSet<T> GetEntity<T>(this IDbContextScope value) where T : class
         {
             return value.DbContexts.Get<ApplicationDbContext>().Set<T>();
+        }
+
+        public static int ExecuteCommand(
+            this IDbContextReadOnlyScope ctx,
+            string query,
+            params object[] parameters
+        )
+        {
+            return ctx.DbContexts.Get<ApplicationDbContext>().Database.ExecuteSqlCommand(query, parameters);
+        }
+
+        public static int ExecuteCommand(
+            this IDbContextScope ctx,
+            string query,
+            params object[] parameters
+        )
+        {
+            return ctx.DbContexts.Get<ApplicationDbContext>().Database.ExecuteSqlCommand(query, parameters);
+        }
+
+        public static IQueryable<T> SqlQuery<T>(
+            this IDbContextReadOnlyScope ctx,
+            string query,
+            params object[] parameters
+        )
+        {
+            return ctx.DbContexts.Get<ApplicationDbContext>().Database.SqlQuery<T>(query, parameters).AsQueryable();
+        }
+
+        public static IQueryable<T> SqlQuery<T>(
+            this IDbContextScope ctx,
+            string query,
+            params object[] parameters
+        )
+        {
+            return ctx.DbContexts.Get<ApplicationDbContext>().Database.SqlQuery<T>(query, parameters).AsQueryable();
         }
     }
 }
